e7d645203b85379a5cd335ae660a26b76a70d05b
Samkanja <68460352+Samkanja@users.noreply.github.com>
2021-01-26 21:36:49 +0300

    changed variable

--- nestedlist.py
+++ nestedlist.py
@@ -4,9 +4,9 @@ for _ in range(int(input())):
     score = float(input())
     students.append([name,score])
 students = sorted(students, key = lambda x:x[1])
-seclowest_score = sorted(list (set([x[1]for x in students])))[1]
+second_lowest_score = sorted(list (set([x[1]for x in students])))[1]
 students2 = []
 for i in students:
-    if i[1] == seclowest_score:
+    if i[1] == second_lowest_score:
         students2.append(i[0])
 print("\n".join(sorted(students2)))
